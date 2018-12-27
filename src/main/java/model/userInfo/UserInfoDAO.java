47b349e835531fa7e08b855a45e333aec386e83e
孫婉萍 <@EEIT>
2018-12-27 11:23:47 +0800

    User DAO

--- src/main/java/model/userInfo/UserInfoDAO.java
+++ src/main/java/model/userInfo/UserInfoDAO.java
@@ -1,7 +1,23 @@
 package model.userInfo;
 
-public class UserInfoDAO {
+import java.util.List;
 
+public interface UserInfoDAO {
 	
-	
+		public abstract UserInfoDAO findByPrimaryKey(String username);
+
+		public abstract List<UserInfoBean> findAll();
+
+		public abstract UserInfoBean create(UserInfoBean bean);
+
+		public abstract UserInfoBean update(byte[] password, String firstname, String lastname, 
+				String identityNo, String email, java.util.Date birth, String sex, String phone,
+				String address, String authority, String googleId, String facebookId,String username);
+
+		public abstract boolean remove(String username);
+		
+//		public abstract boolean update(byte[] password, String firstname, String lastname, 
+//				String identityNo, String email, java.util.Date birth, String sex, String phone,
+//				String address, String authority, String googleId, String facebookId,String username);
+//	
 }
