a39b445766bcda27311db61019a4266e0a5cb7c9
Sherry <sherrysherry92@gmail.com>
2018-12-27 12:17:07 +0800

    寫完所有Bean

--- src/main/java/model/hotel/HotelBean.java
+++ src/main/java/model/hotel/HotelBean.java
@@ -1,5 +1,60 @@
 package model.hotel;
 
-public class HotelBean {
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="Hotel")
+public class HotelBean {
+	@Id
+	private Integer hotelNo;
+	private String hotelName;
+	private String address;
+	private Integer phone;
+	private String email;
+	private Integer totalroom;
+	
+	@Override
+	public String toString() {
+		return "HotelBean [hotelNo=" + hotelNo + ", hotelName=" + hotelName + ", address=" + address + ", phone="
+				+ phone + ", email=" + email + ", totalroom=" + totalroom + "]";
+	}
+	public Integer getHotelNo() {
+		return hotelNo;
+	}
+	public void setHotelNo(Integer hotelNo) {
+		this.hotelNo = hotelNo;
+	}
+	public String getHotelName() {
+		return hotelName;
+	}
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Integer getPhone() {
+		return phone;
+	}
+	public void setPhone(Integer phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Integer getTotalroom() {
+		return totalroom;
+	}
+	public void setTotalroom(Integer totalroom) {
+		this.totalroom = totalroom;
+	}
+	
 }
--- src/main/java/model/hotel/RoomTypeBean.java
+++ src/main/java/model/hotel/RoomTypeBean.java
@@ -1,5 +1,46 @@
 package model.hotel;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="RoomType")
 public class RoomTypeBean {
+	@Id
+	private Integer serialNo;
+	private Integer hotelNo;
+	private Integer roomLeft;
+	private Integer notForSale;
+	
+	@Override
+	public String toString() {
+		return "RoomTypeBean [serialNo=" + serialNo + ", hotelNo=" + hotelNo + ", roomLeft=" + roomLeft
+				+ ", notForSale=" + notForSale + "]";
+	}
+	public Integer getSerialNo() {
+		return serialNo;
+	}
+	public void setSerialNo(Integer serialNo) {
+		this.serialNo = serialNo;
+	}
+	public Integer getHotelNo() {
+		return hotelNo;
+	}
+	public void setHotelNo(Integer hotelNo) {
+		this.hotelNo = hotelNo;
+	}
+	public Integer getRoomLeft() {
+		return roomLeft;
+	}
+	public void setRoomLeft(Integer roomLeft) {
+		this.roomLeft = roomLeft;
+	}
+	public Integer getNotForSale() {
+		return notForSale;
+	}
+	public void setNotForSale(Integer notForSale) {
+		this.notForSale = notForSale;
+	}
 
 }
