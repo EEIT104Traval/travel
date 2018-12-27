d5c1de027a4bf3673fae01dcbc926d4f3853278a
孫婉萍 <@EEIT>
2018-12-27 21:08:18 +0800

    Test

--- src/main/java/misc/SpringJavaConfiguration.java
+++ src/main/java/misc/SpringJavaConfiguration.java
@@ -15,6 +15,9 @@ import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
 import model.flight.FlightOrderInfoBean;
 import model.flight.FlightPassengerInfoBean;
 import model.hotel._01HotelBean;
+import model.rate.RateBean;
+import model.rate.Rate_JPYBean;
+import model.rate.Rate_USDBean;
 import model.ticket.TicketInfoBean;
 import model.ticket.TicketOrderInfoBean;
 import model.tour.GroupTourBean;
@@ -45,7 +48,7 @@ public class SpringJavaConfiguration {
 		LocalSessionFactoryBuilder builder = new LocalSessionFactoryBuilder(dataSource());
 		builder.addAnnotatedClasses(UserInfoBean.class,GroupTourBean.class,TourBatchBean.class,TourMemberInfoBean.class,TourPictureBean.class, TicketInfoBean.class, TicketOrderInfoBean.class, 
 
-		_01HotelBean.class ,FlightOrderInfoBean.class,FlightPassengerInfoBean.class,Rate_USDBean.class,Rate_JPYBean.class);
+		_01HotelBean.class ,FlightOrderInfoBean.class,FlightPassengerInfoBean.class,RateBean.class,Rate_USDBean.class,Rate_JPYBean.class);
 
 
 		Properties props = new Properties();
