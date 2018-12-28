8b22440ce417d10be71fdfd331379e4e297d417a
朱薇 <@163.com>
2018-12-28 15:25:24 +0800

    無話可說因為沒什麼改

--- src/main/java/model/flight/dao/FlightPassengerInfoDAOHibernate.java
+++ src/main/java/model/flight/dao/FlightPassengerInfoDAOHibernate.java
@@ -9,27 +9,27 @@ import org.springframework.stereotype.Repository;
 
 import model.flight.FlightPassengerInfoDAO;
 import model.flight.FlightPassengerInfoBean;
+
 @Repository
-public class FlightPassengerInfoDAOHibernate implements FlightPassengerInfoDAO{
+public class FlightPassengerInfoDAOHibernate implements FlightPassengerInfoDAO {
 	@Autowired
 	private SessionFactory sessionFactory;
 
 	public Session getSession() {
 		return this.sessionFactory.getCurrentSession();
 	}
-	
-	
+
 	@Override
 	public FlightPassengerInfoBean findByPrimaryKey(Integer serialNO) {
-		System.out.println("serialNO"+serialNO);
+		System.out.println("serialNO" + serialNO);
 		return this.getSession().get(FlightPassengerInfoBean.class, serialNO);
 
 	}
 
 	@Override
 	public List<FlightPassengerInfoBean> findAll() {
-		return this.getSession().createQuery("from FlightPassengerInfoBean", FlightPassengerInfoBean.class).setMaxResults(50)
-				.list();
+		return this.getSession().createQuery("from FlightPassengerInfoBean", FlightPassengerInfoBean.class)
+				.setMaxResults(50).list();
 	}
 
 	@Override
@@ -43,8 +43,4 @@ public class FlightPassengerInfoDAOHibernate implements FlightPassengerInfoDAO{
 		return null;
 	}
 
-	
-	
-	
-	
 }
