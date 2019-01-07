895f82034d22d8ee045fb69abe52e321e2721454
姚昭男 <gbs8377@gmail.com>
2019-01-07 21:46:55 +0800

    不知道是service 搞不定還是 controller

--- src/main/java/controller/ticket/TicketController.java
+++ src/main/java/controller/ticket/TicketController.java
@@ -1,15 +1,15 @@
 package controller.ticket;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import model.ticket.TicketInfoBean;
 import model.ticket.TicketInfoService;
@@ -19,24 +19,28 @@ public class TicketController {
 	
 	@Autowired
 	private TicketInfoService ticketInfoService;
-	
+	@ResponseBody
 	@RequestMapping("/voyage/ticket.controller")
-	public void method(String country, Model model) {
-		Locale locale = LocaleContextHolder.getLocale();
+	public String method(@RequestParam(name="country")String country, Model model) {
+	//	Locale locale = LocaleContextHolder.getLocale();
 		System.out.println("country="+country);
 		
 //接收資料
-		Map<String, String> errors = new HashMap<>();
-		model.addAttribute("errors", errors);	
+//驗證資料
 //呼叫model
+		
 		TicketInfoBean bean = new TicketInfoBean();
 		bean.setCountry(country);
-		
+			
 		List<TicketInfoBean> result = ticketInfoService.searchCountry(country);
-		
 		System.out.println(result);
-		
-		
+//呼叫view		
+		if(result == null) {
+			
+			return "voyage/ticket.jsp";
+		}else {
+			return "voyage/ticket.jsp";
+		}
 	
 	}
 }
--- src/main/webapp/後台管理/bindex.jsp
+++ src/main/webapp/後台管理/bindex.jsp
@@ -9,19 +9,7 @@
 <meta name="viewport"
 	content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link href="css/main.css" rel="stylesheet" type="text/css" />
-<!--     <link href="https://fonts.googleapis.com/css?family=Muli:300,400,600,700" rel="stylesheet"> -->
-<!--     <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css"> -->
-<!--     <link rel="stylesheet" href="css/animate.css"> -->
-<!--     <link rel="stylesheet" href="css/owl.carousel.min.css"> -->
-<!--     <link rel="stylesheet" href="css/owl.theme.default.min.css"> -->
-<!--     <link rel="stylesheet" href="css/magnific-popup.css"> -->
-<!--     <link rel="stylesheet" href="css/aos.css"> -->
-<!--     <link rel="stylesheet" href="css/ionicons.min.css"> -->
-<!--     <link rel="stylesheet" href="css/bootstrap-datepicker.css"> -->
-<!--     <link rel="stylesheet" href="css/jquery.timepicker.css"> -->
-<!--     <link rel="stylesheet" href="css/flaticon.css"> -->
-<!--     <link rel="stylesheet" href="css/icomoon.css"> -->
-<!--     <link rel="stylesheet" href="css/style.css"> -->
+
 <script
   src="https://code.jquery.com/jquery-3.3.1.js"integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="crossorigin="anonymous"></script>
 </head>
