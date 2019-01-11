2b14767a68d2a68042b3ca55ade0e8c0290dd9ab
周哲彣 <wen3@gmail.com>
2019-01-11 16:13:01 +0800

    login

--- src/main/java/model/userInfo/UserInfoService.java
+++ src/main/java/model/userInfo/UserInfoService.java
@@ -1,5 +1,6 @@
 package model.userInfo;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,16 +39,38 @@ public class UserInfoService {
 		}	
 	
 	public List<UserInfoBean> findByuserOrderInfo(String user){
-		
 		List<UserInfoBean> userorderinfo = userInfoDAO.findByaccountName(user);
-		System.out.println(userorderinfo);
-				
+		System.out.println(userorderinfo);	
 		if(tdao.finduser(user) != null) {
 //			tdao.foundName(tdao.)
 //			userorderinfo.add(e);
-			
 		}
-
 		return userorderinfo;
 	}
+	
+	public UserInfoBean login(String accountName, String password) {
+		UserInfoBean bean = userInfoDAO.findByPrimaryKey(accountName);
+		if(bean!=null) {
+			if(password!=null && password.length()!=0) {
+				byte[] temp = password.getBytes();	//使用者輸入
+				byte[] pass = bean.getPassword();	//資料庫抓出
+				if(Arrays.equals(temp, pass)) {
+					return bean;
+				}
+			}
+		}
+		return null;
+	}
+	public UserInfoBean changePassword(String username, String oldPassword, String newPassword) {
+		UserInfoBean bean = this.login(username, oldPassword);
+		if(bean!=null) {
+			if (newPassword!=null && newPassword.length()!=0) {
+				byte[] temp = newPassword.getBytes();
+				return userInfoDAO.update(temp, bean.getFirstname(), bean.getLastname(), bean.getIdentityNo(),bean.getEmail(),
+						bean.getBirth(),bean.getSex(),bean.getPhone(),bean.getAddress(),bean.getAuthority(),bean.getGorfb(),bean.getLoginId(),username);
+			}
+		}
+		return bean;
+	}
+
 }
