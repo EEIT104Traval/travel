73e12648597f7d2f0ce129e958362f6e7c2214e7
Sherry <sherrysherry92@gmail.com>
2019-01-21 01:45:17 +0800

    寄信成功！

---
+++ src/main/java/test/EmailSend.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ * https://www.google.com/settings/security/lesssecureapps
+ */
+
+package test;
+
+/**
+ *
+ * @author Naveen
+ */
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.mail.internet.MimeMessage;
+public class EmailSend {
+
+    public static void main(String args[]){
+        try{
+            String host ="smtp.gmail.com" ;
+            String user = "sherrysherry92@gmail.com";
+            String pass = "jxrkaepvctpmffcs";
+            String to = "reese32@hotmail.com.tw";
+            String from = "sherry";
+            String subject = "收到這封信代表我成功寄出了信";
+            String messageText = "Hello! 你好嗎?";
+            boolean sessionDebug = false;
+
+            Properties props = System.getProperties();
+
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.host", host);
+            props.put("mail.smtp.port", "587");
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.required", "true");
+            props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+
+            java.security.Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+            Session mailSession = Session.getDefaultInstance(props, null);
+            mailSession.setDebug(sessionDebug);
+            Message msg = new MimeMessage(mailSession);
+            msg.setFrom(new InternetAddress(from));
+            InternetAddress[] address = {new InternetAddress(to)};
+            msg.setRecipients(Message.RecipientType.TO, address);
+            msg.setSubject(subject); msg.setSentDate(new Date());
+            msg.setText(messageText);
+
+           Transport transport=mailSession.getTransport("smtp");
+           transport.connect(host, user, pass);
+           transport.sendMessage(msg, msg.getAllRecipients());
+           transport.close();
+           System.out.println("message send successfully");
+        }catch(Exception ex)
+        {
+            System.out.println(ex);
+        }
+
+    }
+}
