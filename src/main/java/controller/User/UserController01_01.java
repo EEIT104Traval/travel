56d6044c3410a38146de2f6ef60988b9b96a7c64
姚昭男 <gbs8377@gmail.com>
2019-01-20 13:57:13 +0800

    測試中重開機

--- src/main/java/controller/User/UserController01_01.java
+++ src/main/java/controller/User/UserController01_01.java
@@ -178,24 +178,24 @@ public class UserController01_01 {
 
 	    @RequestMapping(value = "/upload.do", method = RequestMethod.POST)
 	    // 这里的MultipartFile对象变量名跟表单中的file类型的input标签的name相同，所以框架会自动用MultipartFile对象来接收上传过来的文件，当然也可以使用@RequestParam("img")指定其对应的参数名称
-	    public String upload(MultipartFile csv, HttpSession session)
+	    public String upload(MultipartFile csv)
 	            throws Exception {
 	    	System.out.println(csv.toString());
 //	    	System.out.println(session.toString());
 	        // 如果没有文件上传，MultipartFile也不会为null，可以通过调用getSize()方法获取文件的大小来判断是否有上传文件
-	        if (csv.getSize() > 0) {
-	            // 得到项目在服务器的真实根路径，如：/home/tomcat/webapp/项目名/images
-	            String path = session.getServletContext().getRealPath("images");
-	            // 得到文件的原始名称，如：美女.png
-	            String fileName = csv.getOriginalFilename();
-	            // 通过文件的原始名称，可以对上传文件类型做限制，如：只能上传jpg和png的图片文件
-	            if (fileName.endsWith("jpg") || fileName.endsWith("png")) {
-	                File file = new File(path, fileName);
-	                csv.transferTo(file);
-	                return "/success.jsp";
-	            }
-	        }
-	        return "/error.jsp";
+//	        if (csv.getSize() > 0) {
+//	            // 得到项目在服务器的真实根路径，如：/home/tomcat/webapp/项目名/images
+//	            String path = session.getServletContext().getRealPath("images");
+//	            // 得到文件的原始名称，如：美女.png
+//	            String fileName = csv.getOriginalFilename();
+//	            // 通过文件的原始名称，可以对上传文件类型做限制，如：只能上传jpg和png的图片文件
+//	            if (fileName.endsWith("jpg") || fileName.endsWith("png")) {
+//	                File file = new File(path, fileName);
+//	                csv.transferTo(file);
+//	                return "/success.jsp";
+//	            }
+//	        }
+	        return "接收";
 	    }
 	}
 }
--- src/main/webapp/後台管理/04_01.jsp
+++ src/main/webapp/後台管理/04_01.jsp
@@ -1,5 +1,9 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
+		<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"
+	integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+	crossorigin="anonymous"></script>
 <!DOCTYPE html>
 <html>
 <head>
@@ -15,6 +19,7 @@
 			<br /> 
 			<input type="submit" name="提交">
 		</form>
+		
 	</div>
 	<div class="boxmsg" id="searchuser">
 		<!-- 	----------------------加東西--------------------- -->
