c2b1e74d3eaeaee9a3b2d9aef384768885d3adb7
Sherry <sherrysherry92@gmail.com>
2019-01-22 10:17:23 +0800

    Push 好做事

--- src/main/java/model/hotel/HotelOrderDetailsService.java
+++ src/main/java/model/hotel/HotelOrderDetailsService.java
@@ -5,14 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 @Service
+
 public class HotelOrderDetailsService {
 	
 @Autowired
 private HotelOrderDetailsDAO hotelOrderDetailsDAO = null;
-
-	
-	
-
 
 	//查詢訂單------------------------後台寫的---------------------------
 	public List<HotelOrderDetailsBean> foundOrderaccountName(String accountName) {
--- src/main/java/other/EmailSend.java
+++ src/main/java/other/EmailSend.java
@@ -10,9 +10,12 @@ package other;
  *
  * @author Naveen
  */
-import java.util.*;
-import javax.mail.*;
-import javax.mail.internet.*;
+import java.util.Date;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 public class EmailSend {
 
@@ -46,10 +49,35 @@ public class EmailSend {
             msg.setSubject(subject); msg.setSentDate(new Date());
 //            msg.setText(messageText);
             
-            msg.setContent(
-                    "<h1>訂單明細</h1>",
-                   "text/html");
-
+            String message = "<div style='font-family: Microsoft JhengHei'>"
+            +"<h1>Time To Travel</h1>"
+            +"<p>Hello! 訂購人，</p>"
+            +"<p>我們已收到您的訂單 #20190202001</p>"
+            +"<br><br>"
+            +"<b>訂單明細</b>"
+            +"<p>訂單編號:　#19010922153JV4S</p>"
+            +"<p>訂單日期:　2019-01-09 22:15:20 +0800</p>"
+            +"<p>訂購人：　XXX</p>"
+            +"<br>"
+            +"<p>飯店名稱: 紐約市中心希爾頓酒店 (New York Hilton Midtown) - 總統轉角套房</p>"
+            +"<img alt='總統轉角套房'"
+            +"<img alt='總統轉角套房' "
+            + "src='https://secure3.hilton.com/resources/media/hi/NYCNHHH/en_US/img/hotel/roomtypeimages/main/HH_prescnr01_386x310_FitToBoxSmallDimension_Center.jpg' "
+            + "style='width: auto;'"+ "'/>'"
+            +"<p>地址: 1335 Avenue of the Americas</p>"
+            +"<p>日期: 2019-02-02 ~ 2019-02-04 (共2晚)</p>"
+            +"<p>總金額: NT$ 149,332</p>"
+            +"<hr>"
+            +"<p>感謝您的訂購！</p>"
+            +"<a href='http://localhost:8080/Travel/voyage/index.jsp'>修改訂單</a>"
+            +"</div>"
+            +"<hr>"
+            +"<div style='text-align: center'>"
+            +"<p>Copyright © 1999-2019 Time To Travel All rights reserved<p>"
+            +"</div>"
+            +"<div style=\"color:red;\">BRIDGEYE</div>";
+            msg.setContent(message, "text/html; charset=utf-8");
+            
            Transport transport=mailSession.getTransport("smtp");
            transport.connect(host, user, pass);
            transport.sendMessage(msg, msg.getAllRecipients());
